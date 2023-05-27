a1569dd3c1fbe20f4f0c0d2cd6301461f9890c1b
Alaric <>
2023-05-26 22:43:15 -0700

    Change variable names to match convention

--- examples/npatch_drawing.zig
+++ examples/npatch_drawing.zig
@@ -25,32 +25,34 @@ const Color = raylib.Color;
 pub fn main() !void {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const screen_width = 800;
+    const screen_height = 450;
 
-    raylib.initWindow(screenWidth, screenHeight, "raylib [textures] example - N-patch drawing");
+    raylib.initWindow(screen_width, screen_height, "raylib [textures] example - N-patch drawing");
+    defer raylib.closeWindow(); // Close window and OpenGL context
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    const nPatchTexture = try raylib.Texture2D.init("resources/ninepatch_button.png");
+    const npatch_texture = try raylib.Texture2D.init("resources/ninepatch_button.png");
+    defer npatch_texture.deinit();
 
-    var mousePosition: raylib.Vector2 = undefined;
+    var mouse_position: raylib.Vector2 = undefined;
     const origin = .{ .x = 0, .y = 0 };
 
     // Position and size of the n-patches
-    var dstRec1: raylib.Rectangle = .{ .x = 480, .y = 160, .width = 32, .height = 32 };
-    var dstRec2: raylib.Rectangle = .{ .x = 160, .y = 160, .width = 32, .height = 32 };
-    var dstRecH: raylib.Rectangle = .{ .x = 160, .y = 93, .width = 32, .height = 32 };
-    var dstRecV: raylib.Rectangle = .{ .x = 92, .y = 160, .width = 32, .height = 32 };
+    var dst_rec_1: raylib.Rectangle = .{ .x = 480, .y = 160, .width = 32, .height = 32 };
+    var dst_rec_2: raylib.Rectangle = .{ .x = 160, .y = 160, .width = 32, .height = 32 };
+    var dst_rec_h: raylib.Rectangle = .{ .x = 160, .y = 93, .width = 32, .height = 32 };
+    var dst_rec_v: raylib.Rectangle = .{ .x = 92, .y = 160, .width = 32, .height = 32 };
 
     // A 9-patch (NPATCH_NINE_PATCH) changes its sizes in both axis
-    const ninePatchInfo1 = .{ .source = .{ .x = 0, .y = 0, .width = 64, .height = 64 }, .left = 12, .top = 40, .right = 12, .bottom = 12, .layout = NPatchLayout.nine_patch };
-    const ninePatchInfo2 = .{ .source = .{ .x = 0, .y = 128, .width = 64, .height = 64 }, .left = 16, .top = 16, .right = 16, .bottom = 16, .layout = NPatchLayout.nine_patch };
+    const nine_patch_info_1 = .{ .source = .{ .x = 0, .y = 0, .width = 64, .height = 64 }, .left = 12, .top = 40, .right = 12, .bottom = 12, .layout = NPatchLayout.nine_patch };
+    const nine_patch_info_2 = .{ .source = .{ .x = 0, .y = 128, .width = 64, .height = 64 }, .left = 16, .top = 16, .right = 16, .bottom = 16, .layout = NPatchLayout.nine_patch };
 
     // A horizontal 3-patch (NPATCH_THREE_PATCH_HORIZONTAL) changes its sizes along the x axis only
-    const h3PatchInfo = .{ .source = .{ .x = 0, .y = 64, .width = 64, .height = 64 }, .left = 8, .top = 8, .right = 8, .bottom = 8, .layout = NPatchLayout.three_patch_horizontal };
+    const h3_patch_info = .{ .source = .{ .x = 0, .y = 64, .width = 64, .height = 64 }, .left = 8, .top = 8, .right = 8, .bottom = 8, .layout = NPatchLayout.three_patch_horizontal };
 
     // A vertical 3-patch (NPATCH_THREE_PATCH_VERTICAL) changes its sizes along the y axis only
-    const v3PatchInfo = .{ .source = .{ .x = 0, .y = 192, .width = 64, .height = 64 }, .left = 6, .top = 6, .right = 6, .bottom = 6, .layout = NPatchLayout.three_patch_vertical };
+    const v3_patch_info = .{ .source = .{ .x = 0, .y = 192, .width = 64, .height = 64 }, .left = 6, .top = 6, .right = 6, .bottom = 6, .layout = NPatchLayout.three_patch_vertical };
 
     raylib.setTargetFPS(60);
     //---------------------------------------------------------------------------------------
@@ -60,54 +62,47 @@ pub fn main() !void {
     {
         // Update
         //----------------------------------------------------------------------------------
-        mousePosition = raylib.getMousePosition();
+        mouse_position = raylib.getMousePosition();
 
         // Resize the n-patches based on mouse position
-        dstRec1.width = mousePosition.x - dstRec1.x;
-        dstRec1.height = mousePosition.y - dstRec1.y;
-        dstRec2.width = mousePosition.x - dstRec2.x;
-        dstRec2.height = mousePosition.y - dstRec2.y;
-        dstRecH.width = mousePosition.x - dstRecH.x;
-        dstRecV.height = mousePosition.y - dstRecV.y;
+        dst_rec_1.width = mouse_position.x - dst_rec_1.x;
+        dst_rec_1.height = mouse_position.y - dst_rec_1.y;
+        dst_rec_2.width = mouse_position.x - dst_rec_2.x;
+        dst_rec_2.height = mouse_position.y - dst_rec_2.y;
+        dst_rec_h.width = mouse_position.x - dst_rec_h.x;
+        dst_rec_v.height = mouse_position.y - dst_rec_v.y;
 
         // Set a minimum width and/or height
-        if (dstRec1.width < 1) dstRec1.width = 1;
-        if (dstRec1.width > 300) dstRec1.width = 300;
-        if (dstRec1.height < 1) dstRec1.height = 1;
-        if (dstRec2.width < 1) dstRec2.width = 1;
-        if (dstRec2.width > 300) dstRec2.width = 300;
-        if (dstRec2.height < 1) dstRec2.height = 1;
-        if (dstRecH.width < 1) dstRecH.width = 1;
-        if (dstRecV.height < 1) dstRecV.height = 1;
+        if (dst_rec_1.width < 1) dst_rec_1.width = 1;
+        if (dst_rec_1.width > 300) dst_rec_1.width = 300;
+        if (dst_rec_1.height < 1) dst_rec_1.height = 1;
+        if (dst_rec_2.width < 1) dst_rec_2.width = 1;
+        if (dst_rec_2.width > 300) dst_rec_2.width = 300;
+        if (dst_rec_2.height < 1) dst_rec_2.height = 1;
+        if (dst_rec_h.width < 1) dst_rec_h.width = 1;
+        if (dst_rec_v.height < 1) dst_rec_v.height = 1;
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         raylib.beginDrawing();
+        defer raylib.endDrawing();
 
         raylib.clearBackground(Color.ray_white);
 
         // Draw the n-patches
-        nPatchTexture.drawNPatch(ninePatchInfo2, dstRec2, origin, 0, Color.white);
-        nPatchTexture.drawNPatch(ninePatchInfo1, dstRec1, origin, 0, Color.white);
-        nPatchTexture.drawNPatch(h3PatchInfo, dstRecH, origin, 0, Color.white);
-        nPatchTexture.drawNPatch(v3PatchInfo, dstRecV, origin, 0, Color.white);
+        npatch_texture.drawNPatch(nine_patch_info_2, dst_rec_2, origin, 0, Color.white);
+        npatch_texture.drawNPatch(nine_patch_info_1, dst_rec_1, origin, 0, Color.white);
+        npatch_texture.drawNPatch(h3_patch_info, dst_rec_h, origin, 0, Color.white);
+        npatch_texture.drawNPatch(v3_patch_info, dst_rec_v, origin, 0, Color.white);
 
         // Draw the source texture
         raylib.drawRectangleLines(5, 88, 74, 266, Color.blue);
-        nPatchTexture.draw(10, 93, Color.white);
+        npatch_texture.draw(10, 93, Color.white);
         raylib.drawText("TEXTURE", 15, 360, 10, Color.dark_gray);
 
         raylib.drawText("Move the mouse to stretch or shrink the n-patches", 10, 20, 20, Color.dark_gray);
 
-        raylib.endDrawing();
         //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    nPatchTexture.deinit();
-
-    raylib.closeWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 }
